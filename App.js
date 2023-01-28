8d2d88253b40b5073df39de49536fb5f1b6c0822
marcocrupi <marcocrupi@hotmail.it>
2023-01-28 12:18:07 +0100

    add FlatList

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   TextInput,
   View,
   ScrollView,
+  FlatList,
 } from "react-native";
 
 export default function App() {
@@ -23,7 +24,19 @@ export default function App() {
   function addGoalHandler() {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      enteredGoalText,
+
+      {
+        text: enteredGoalText,
+        // Soluzione per creare una chiave univoca, "key" che contiene il valore restituito da Date.now() combinato con Math.random().
+        // In questo modo si utilizzano due fonti di casualità, la data corrente e un
+        // numero casuale generato da Math.random() per generare una chiave univoca.
+        // Tenere presente che Math.random() genera un numero casuale tra 0 e 1, quindi in questo caso si devono
+        // fare alcune conversioni per evitare conflitti tra le chiavi generate.
+        // In questo caso si utilizza il metodo toString(radix) con un valore di radice di 36
+        // per convertire il numero in una stringa alfanumerica, quindi si utilizza il metodo substring(start,end)
+        // per prendere solo i primi caratteri della stringa.
+        id: Date.now() + Math.random().toString(36).substring(2, 15),
+      },
     ]);
   }
 
@@ -39,14 +52,32 @@ export default function App() {
       </View>
       <View style={styles.goalsContainer}>
         {/* ScrollView serve quando ci serve la funzionalità di scroll, a questo link
-        un approfondimento per personalizzarlo: https://reactnative.dev/docs/scrollview */}
-        <ScrollView alwaysBounceVertical={false}>
-          {courseGoals.map((goal, i) => (
-            <View style={styles.goalItem} key={i}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          ))}
-        </ScrollView>
+        un approfondimento per personalizzarlo: https://reactnative.dev/docs/scrollview
+        Il problema di ScrollView è che renderizza tutti gli elementi, ed è un
+        problema se essi sono tanti, è meglio usare FlatList per molto elementi,
+        per la FlatList abbiamo modificato molto il codice che usava il metodo map, andare a
+        vedere il commit inerente la scroll view */}
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            console.log(itemData);
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          // Il codice utilizza la propietà "keyExtractor" di FlatList per specificare come vengono estratte
+          // le chiavi univoche per gli elementi della lista. In questo caso, la funzione
+          // passata come valore per "keyExtractor" restituisce il valore della proprietà "id"
+          // dell'oggetto "item". Ciò consente a FlatList di associare una chiave univoca
+          // per ogni elemento della lista, migliorando le prestazioni e l'efficiente
+          // gestione degli elementi della lista.
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+        />
       </View>
     </View>
   );
