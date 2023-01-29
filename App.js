c285c9413649d92d265926b83f5d49b69270400c
marcocrupi <marcocrupi@hotmail.it>
2023-01-29 18:10:05 +0100

    add status bar

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { StyleSheet, View, FlatList, Button } from "react-native";
 
+// Ci permette di personalizzare il look della status bar
+import { StatusBar } from "expo-status-bar";
+
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
@@ -46,48 +49,51 @@ export default function App() {
   }
 
   return (
-    <View style={styles.appContainer}>
-      <Button
-        title="Add New Goal"
-        color="#210644"
-        onPress={startAddGoalHandler}
-      />
-      <GoalInput
-        visible={modalIsVisible}
-        onAddGoal={addGoalHandler}
-        onCancel={endAddGoalHandler}
-      />
-      <View style={styles.goalsContainer}>
-        {/* ScrollView serve quando ci serve la funzionalità di scroll, a questo link
+    <>
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        <Button
+          title="Add New Goal"
+          color="#a065ec"
+          onPress={startAddGoalHandler}
+        />
+        <GoalInput
+          visible={modalIsVisible}
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
+        <View style={styles.goalsContainer}>
+          {/* ScrollView serve quando ci serve la funzionalità di scroll, a questo link
         un approfondimento per personalizzarlo: https://reactnative.dev/docs/scrollview
         Il problema di ScrollView è che renderizza tutti gli elementi, ed è un
         problema se essi sono tanti, è meglio usare FlatList per molto elementi,
         per la FlatList abbiamo modificato molto il codice che usava il metodo map, andare a
         vedere il commit inerente la scroll view */}
-        <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => {
-            return (
-              <GoalItem
-                text={itemData.item.text}
-                id={itemData.item.id}
-                onDeleteItem={deleteGoalHandler}
-              />
-            );
-          }}
-          // Il codice utilizza la propietà "keyExtractor" di FlatList per specificare come vengono estratte
-          // le chiavi univoche per gli elementi della lista. In questo caso, la funzione
-          // passata come valore per "keyExtractor" restituisce il valore della proprietà "id"
-          // dell'oggetto "item". Ciò consente a FlatList di associare una chiave univoca
-          // per ogni elemento della lista, migliorando le prestazioni e l'efficiente
-          // gestione degli elementi della lista.
-          keyExtractor={(item, index) => {
-            return item.id;
-          }}
-          alwaysBounceVertical={false}
-        />
+          <FlatList
+            data={courseGoals}
+            renderItem={(itemData) => {
+              return (
+                <GoalItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteGoalHandler}
+                />
+              );
+            }}
+            // Il codice utilizza la propietà "keyExtractor" di FlatList per specificare come vengono estratte
+            // le chiavi univoche per gli elementi della lista. In questo caso, la funzione
+            // passata come valore per "keyExtractor" restituisce il valore della proprietà "id"
+            // dell'oggetto "item". Ciò consente a FlatList di associare una chiave univoca
+            // per ogni elemento della lista, migliorando le prestazioni e l'efficiente
+            // gestione degli elementi della lista.
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+            alwaysBounceVertical={false}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -35,10 +35,10 @@ function GoalInput(props) {
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
+            <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0" />
           </View>
           <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancel} />
+            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
           </View>
         </View>
       </View>
@@ -63,7 +63,10 @@ const styles = StyleSheet.create({
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "#cccccc",
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
+    color: "#120438",
+    borderRadius: 6,
     width: "100%",
     padding: 8,
   },
